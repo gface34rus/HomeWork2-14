6bb194a1d45a382079bfba81c63235f1043834ae
gface <89259112970@mail.ru>
2024-10-09 18:36:34 +0300

    Initial commit

---
+++ src/test/java/pro/sky/homework2_14/Homework214ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homework2_14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework214ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
